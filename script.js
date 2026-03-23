2ebaae735b627a9bca3059e72dc89f7e5ec820e3
guioliveira2025 <guiandrade.oliveira@hotmail.com>
2026-03-22 21:28:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+body {
+font-family: Arial;
+margin: 0;
+background: #f4f4f4;
+}
+
+header {
+background: #222;
+color: white;
+padding: 15px;
+text-align: center;
+}
+
+nav a {
+margin: 10px;
+color: white;
+text-decoration: none;
+}
+
+section {
+padding: 30px;
+}
+
+form {
+display: flex;
+flex-direction: column;
+width: 300px;
+}
+
+input, textarea {
+margin: 10px 0;
+padding: 10px;
+}
+
+.dark {
+background: #121212;
+color: white;
+}
